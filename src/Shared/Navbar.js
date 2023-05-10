676f4fec20cacef3f351f094d38dfa2c20447aa2
Ohi20 <ashrafohi20@gmail.com>
2023-05-10 12:30:53 +0600

    Navbar-Modified

--- src/Shared/Navbar.js
+++ src/Shared/Navbar.js
@@ -5,12 +5,11 @@ import { useAuthState } from 'react-firebase-hooks/auth';
 import auth from '../firebase.init';
 import { signOut } from 'firebase/auth';
 
+// variables for theme change
 const THEMES = ["light", "dark", "cupcake", "bumblebee", "emerald", "corporate", "synthwave", "retro", "cyberpunk", "valentine", "halloween", "garden", "forest", "aqua", "lofi", "pastel", "fantasy", "wireframe", "black", "luxury", "dracula", "cmyk", "autumn", "business", "acid", "lemonade", "night", "coffee", "winter"];
 
 const Navbar = () => {
-
   const [user, loading, error] = useAuthState(auth);
-
   const [theme, setTheme] = useState('light');
 
   useEffect(() => {
@@ -23,13 +22,9 @@ const Navbar = () => {
   }
 
   // const userName = user.displayName;
-
-
   const logout = () => {
     signOut(auth);
   }
-
-
 
   return (
     <header className='bg-base-100 py-2 sticky top-0 z-50'>
@@ -40,6 +35,7 @@ const Navbar = () => {
               <label tabIndex={0} className="btn btn-circle btn-primary lg:hidden mr-1">
                 <i className='bi bi-list text-2xl'></i>
               </label>
+              {/* lobile content */}
               <ul tabIndex={0} className="dropdown-content mt-1 w-52 menu menu-compact p-2 bg-base-200 shadow rounded-box">
                 <li><Link to='/'>Home</Link></li>
                 <li><Link to='/purchase'>Purchase</Link></li>
@@ -48,10 +44,11 @@ const Navbar = () => {
                 <li>{user ? <button onClick={logout} className="btn btn-error text-white">Sign Out</button> : <Link to="/login">Login</Link>}</li>
               </ul>
             </div>
-            <a className="btn btn-ghost normal-case text-2xl">daisyUI</a>
+            <img className='invisible lg:visible md:visible' src={logo1} alt='' />
           </div>
           <div className="navbar-center hidden lg:flex">
             <ul className="menu menu-horizontal p-0 font-medium">
+              {/* large content */}
               <li><Link to='/'>Home</Link></li>
               <li><Link to='/purchase'>Purchase</Link></li>
               <li><Link to='/review'>Review</Link></li>
@@ -59,10 +56,12 @@ const Navbar = () => {
               <li>{user ? <button onClick={logout} className="btn btn-error text-white">Sign Out</button> : <Link to="/login">Login</Link>}</li>
             </ul>
           </div>
+
+          {/* theme */}
           <div className="navbar-end">
             <div className="dropdown dropdown-end">
               <label tabIndex={0} className="btn">
-                {THEMES.length} Themes
+                Change Themes
               </label>
               <ul tabIndex={0} className="dropdown-content mt-1 w-52 max-h-96 overflow-y-auto menu menu-compact p-2  bg-base-200 shadow rounded-box">
                 {
