d331743285cb114a2b9423a22c167fb4b45579ed
Ohi20 <ashrafohi20@gmail.com>
2023-05-10 12:55:28 +0600

    Banner-modified

--- src/Pages/Home/Homebanner.js
+++ src/Pages/Home/Homebanner.js
@@ -1,21 +1,22 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Homebanner = () => {
     return (
       <section>
       <div className='container'>
-        <div className="hero h-96 md:h-[500px] rounded-box overflow-hidden" style={{ backgroundImage: `url("https://placeimg.com/1000/800/tech")` }}>
-          <div className="hero-overlay bg-opacity-60 bg-secondary"></div>
+        <div className="hero h-96 md:h-[500px] rounded-box overflow-hidden" style={{ backgroundImage: `url("https://webneel.com/wallpaper/sites/default/files/images/03-2014/5-art-wallpaper.jpg")` }}>
+          <div className="hero-overlay bg-opacity-50 bg-secondary"></div>
           <div className="hero-content text-center text-secondary-content">
             <div className="max-w-lg">
               <h1 className="mb-5 sm:mb-7 text-4xl sm:text-5xl font-bold">
                 Let's make your
-                product a success
+                art mind a success
               </h1>
               <p className="mb-5 sm:mb-7 sm:text-lg">
-                Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo.
+                Welcome to possibilitarian world
               </p>
-              <button className="btn btn-warning sm:btn-wide">Get Started</button>
+              <button className="btn btn-warning sm:btn-wide"><Link to='/purchase'>Get Started</Link></button>
             </div>
           </div>
         </div>
--- src/Shared/Navbar.js
+++ src/Shared/Navbar.js
@@ -9,7 +9,7 @@ import { signOut } from 'firebase/auth';
 const THEMES = ["light", "dark", "cupcake", "bumblebee", "emerald", "corporate", "synthwave", "retro", "cyberpunk", "valentine", "halloween", "garden", "forest", "aqua", "lofi", "pastel", "fantasy", "wireframe", "black", "luxury", "dracula", "cmyk", "autumn", "business", "acid", "lemonade", "night", "coffee", "winter"];
 
 const Navbar = () => {
-  const [user, loading, error] = useAuthState(auth);
+  const [user] = useAuthState(auth);
   const [theme, setTheme] = useState('light');
 
   useEffect(() => {
@@ -44,7 +44,7 @@ const Navbar = () => {
                 <li>{user ? <button onClick={logout} className="btn btn-error text-white">Sign Out</button> : <Link to="/login">Login</Link>}</li>
               </ul>
             </div>
-            <img className='invisible lg:visible md:visible' src={logo1} alt='' />
+            <a className="btn btn-ghost normal-case text-2xl">BulkArt</a>
           </div>
           <div className="navbar-center hidden lg:flex">
             <ul className="menu menu-horizontal p-0 font-medium">
@@ -61,7 +61,10 @@ const Navbar = () => {
           <div className="navbar-end">
             <div className="dropdown dropdown-end">
               <label tabIndex={0} className="btn">
-                Change Themes
+                <svg className="text-white" xmlns="http://www.w3.org/2000/svg" fill = '#fff' width="32" height="32" viewBox="0 0 32 32" aria-labelledby="title">
+                  <title id="title">Umbrella Icon</title>
+                  <path d="M27 14h5c0-1.105-1.119-2-2.5-2s-2.5 0.895-2.5 2v0zM27 14c0-1.105-1.119-2-2.5-2s-2.5 0.895-2.5 2c0-1.105-1.119-2-2.5-2s-2.5 0.895-2.5 2v0 14c0 1.112-0.895 2-2 2-1.112 0-2-0.896-2-2.001v-1.494c0-0.291 0.224-0.505 0.5-0.505 0.268 0 0.5 0.226 0.5 0.505v1.505c0 0.547 0.444 0.991 1 0.991 0.552 0 1-0.451 1-0.991v-14.009c0-1.105-1.119-2-2.5-2s-2.5 0.895-2.5 2c0-1.105-1.119-2-2.5-2s-2.5 0.895-2.5 2c0-1.105-1.119-2-2.5-2s-2.5 0.895-2.5 2c0-5.415 6.671-9.825 15-9.995v-1.506c0-0.283 0.224-0.499 0.5-0.499 0.268 0 0.5 0.224 0.5 0.499v1.506c8.329 0.17 15 4.58 15 9.995h-5z" />
+                </svg>
               </label>
               <ul tabIndex={0} className="dropdown-content mt-1 w-52 max-h-96 overflow-y-auto menu menu-compact p-2  bg-base-200 shadow rounded-box">
                 {
