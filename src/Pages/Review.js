e12a00e0ba626c631379d55099e6fb225cc8bf76
Ohi20 <ashrafohi20@gmail.com>
2023-05-10 15:00:21 +0600

    Review-Page-Added

--- src/Pages/Review.js
+++ src/Pages/Review.js
@@ -1,175 +1,181 @@
 import React from 'react';
+import Footer from '../Shared/Footer';
 
 const Review = () => {
     return (
-        <section className='my-10 py-10 md:py-16 bg-neutral'>
-        <div className='container'>
-          <div className='text-center text-white'>
-            <h2 className='text-3xl sm:text-5xl font-bold mb-4'>We have some fans.</h2>
-            <p className='text-lg sm:text-2xl mb-6 md:mb-14'>
-              We create engaging experiences that are innovatingand beautiful.
-            </p>
+      <section className='my-10 py-10 md:py-16 bg-neutral'>
+      <div className='container'>
+        <div className='text-center text-white'>
+          <h2 className='text-3xl sm:text-5xl font-bold mb-4'>Reviews from People</h2>
+          <p className='text-lg sm:text-2xl mb-6 md:mb-14'>
+            We create engaging experiences that are innovatingand beautiful.
+          </p>
+        </div>
+
+        <div className='columns-1 sm:columns-2 lg:columns-3 gap-6 lg:gap-8'>
+          <div className='block mb-8 lg:mb-10'>
+            <div className='stack'>
+              <div className="card bg-secondary text-secondary-content">
+                <div className="card-body gap-4">
+                  <div className='flex items-center'>
+                    <div className="avatar flex-shrink-0">
+                      <div className="w-16 rounded-full border-2 border-white">
+                        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRm0R_u9-nelm6V3ASlL2v5maZpQ2_cideCx37wg-YGto270-m0lOHpQEv9uZzmHhadFGA&usqp=CAU" />
+                      </div>
+                    </div>
+
+                    <div className='ml-3'>
+                      <p className='text-lg font-medium leading-tight'>Eric Ampire</p>
+                      <small>Customer</small>
+                    </div>
+                  </div>
+                  <p>
+                    “I first encountered this work when I interviewed her for the Sunday Times ‘Style’ magazine. I was quite simply blown away by her ability to translate raw emotion and energy onto the canvas, creating whole domains of colour and texture that touch a nerve deep inside our own psyche. Because her work is not constrained by obvious figurative or landscape content, the eye is free to explore the layers within and discover its own worlds – no wonder then that she is the artist of choice for many leading, international interior designers, as her paintings sit as happily in very simple, contemporary interiors as in the lavish and the opulent.”
+                  </p>
+                </div>
+              </div>
+              <div className='card bg-primary h-10'></div>
+              <div className='card bg-warning h-10'></div>
+            </div>
           </div>
 
-          <div className='columns-1 sm:columns-2 lg:columns-3 gap-6 lg:gap-8'>
-            <div className='block mb-8 lg:mb-10'>
-              <div className='stack'>
-                <div className="card bg-secondary text-secondary-content">
-                  <div className="card-body gap-4">
-                    <div className='flex items-center'>
-                      <div className="avatar flex-shrink-0">
-                        <div className="w-16 rounded-full border-2 border-white">
-                          <img src="https://placeimg.com/100/100/people" />
-                        </div>
-                      </div>
-
-                      <div className='ml-3'>
-                        <p className='text-lg font-medium leading-tight'>Eric Ampire</p>
-                        <small>Mobile dev</small>
+          <div className='block mb-8 lg:mb-10'>
+            <div className='stack'>
+              <div className="card bg-secondary text-secondary-content">
+                <div className="card-body gap-4">
+                  <div className='flex items-center'>
+                    <div className="avatar flex-shrink-0">
+                      <div className="w-16 rounded-full border-2 border-white">
+                        <img src="https://onemarketmedia.com/wp-content/uploads/2020/06/testimonial-video.jpg" />
                       </div>
                     </div>
-                    <p>
-                      Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum aliquid quo eum quae quos illo earum ipsa doloribus nostrum minus libero aspernatur laborum cum, a suscipit, ratione ea totam ullam! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Architecto laboriosam deleniti aperiam ab veniam sint non cumque quis tempore cupiditate. Sint libero voluptas veniam at reprehenderit, veritatis harum et rerum.
-                    </p>
+
+                    <div className='ml-3'>
+                      <p className='text-lg font-medium leading-tight'>Adam Wathan</p>
+                      <small>Manager</small>
+                    </div>
                   </div>
+                  <p>
+
+                    “They create abstract worlds in which the imagination can roam freely and pleasurably through deeply-layered, impressionistic landscapes. I find her work both emotive and engaging.”
+                  </p>
                 </div>
-                <div className='card bg-primary h-10'></div>
-                <div className='card bg-warning h-10'></div>
               </div>
+              <div className='card bg-primary h-10'></div>
+              <div className='card bg-warning h-10'></div>
             </div>
+          </div>
 
-            <div className='block mb-8 lg:mb-10'>
-              <div className='stack'>
-                <div className="card bg-secondary text-secondary-content">
-                  <div className="card-body gap-4">
-                    <div className='flex items-center'>
-                      <div className="avatar flex-shrink-0">
-                        <div className="w-16 rounded-full border-2 border-white">
-                          <img src="https://placeimg.com/99/99/people" />
-                        </div>
-                      </div>
-
-                      <div className='ml-3'>
-                        <p className='text-lg font-medium leading-tight'>Adam Wathan</p>
-                        <small>Creator of Tailwindcss</small>
+          <div className='block mb-8 lg:mb-10'>
+            <div className='stack'>
+              <div className="card bg-secondary text-secondary-content">
+                <div className="card-body gap-4">
+                  <div className='flex items-center'>
+                    <div className="avatar flex-shrink-0">
+                      <div className="w-16 rounded-full border-2 border-white">
+                        <img src="https://www.shutterstock.com/image-photo/studio-portrait-happy-woman-long-260nw-1954427407.jpg" />
                       </div>
                     </div>
-                    <p>
-                      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Architecto laboriosam deleniti aperiam ab veniam sint non cumque quis tempore cupiditate. Sint libero voluptas veniam at reprehenderit, veritatis harum et rerum.
-                    </p>
+
+                    <div className='ml-3'>
+                      <p className='text-lg font-medium leading-tight'>Gal Gadot</p>
+                      <small>Customer</small>
+                    </div>
                   </div>
+                  <p>
+                    “Zoob’s use of line, texture and colour recall the work of Gerhard Richter’s abstract paintings and the poetic quality of Claude Monet’s later works. However, they are rich in imagination with an innate energy and beauty all of their own.”
+                  </p>
                 </div>
-                <div className='card bg-primary h-10'></div>
-                <div className='card bg-warning h-10'></div>
               </div>
+              <div className='card bg-primary h-10'></div>
+              <div className='card bg-warning h-10'></div>
             </div>
+          </div>
 
-            <div className='block mb-8 lg:mb-10'>
-              <div className='stack'>
-                <div className="card bg-secondary text-secondary-content">
-                  <div className="card-body gap-4">
-                    <div className='flex items-center'>
-                      <div className="avatar flex-shrink-0">
-                        <div className="w-16 rounded-full border-2 border-white">
-                          <img src="https://placeimg.com/98/98/people" />
-                        </div>
-                      </div>
-
-                      <div className='ml-3'>
-                        <p className='text-lg font-medium leading-tight'>Ben Sulumu</p>
-                        <small>Manager</small>
+          <div className='block mb-8 lg:mb-10'>
+            <div className='stack'>
+              <div className="card bg-secondary text-secondary-content">
+                <div className="card-body gap-4">
+                  <div className='flex items-center'>
+                    <div className="avatar flex-shrink-0">
+                      <div className="w-16 rounded-full border-2 border-white">
+                        <img src="https://blog.hubspot.com/hubfs/Customer-testimonial-page.jpg" />
                       </div>
                     </div>
-                    <p>
-                      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Architecto laboriosam deleniti aperiam ab veniam sint non cumque quis tempore cupiditate. Sint libero voluptas veniam at reprehenderit, veritatis harum et rerum.
-                    </p>
+
+                    <div className='ml-3'>
+                      <p className='text-lg font-medium leading-tight'>Sabrina Aguilar</p>
+                      <small>Customer</small>
+                    </div>
                   </div>
+                  <p>
+
+                    “I was searching for a special artwork to feature in the atrium of an important clients’ Mediterranean villa. I contacted Jessica immediately. The triptych painting she produced for me, in her signature abstract style, creates an incredible atmosphere in the space. I love her exuberant use of texture, colour and pattern.”
+                  </p>
                 </div>
-                <div className='card bg-primary h-10'></div>
-                <div className='card bg-warning h-10'></div>
               </div>
+              <div className='card bg-primary h-10'></div>
+              <div className='card bg-warning h-10'></div>
             </div>
+          </div>
 
-            <div className='block mb-8 lg:mb-10'>
-              <div className='stack'>
-                <div className="card bg-secondary text-secondary-content">
-                  <div className="card-body gap-4">
-                    <div className='flex items-center'>
-                      <div className="avatar flex-shrink-0">
-                        <div className="w-16 rounded-full border-2 border-white">
-                          <img src="https://placeimg.com/97/97/people" />
-                        </div>
-                      </div>
-
-                      <div className='ml-3'>
-                        <p className='text-lg font-medium leading-tight'>Rodrigo Aguilar</p>
-                        <small>Creator of Tailwind Awesome</small>
+          <div className='block mb-8 lg:mb-10'>
+            <div className='stack'>
+              <div className="card bg-secondary text-secondary-content">
+                <div className="card-body gap-4">
+                  <div className='flex items-center'>
+                    <div className="avatar flex-shrink-0">
+                      <div className="w-16 rounded-full border-2 border-white">
+                        <img src="https://www.shutterstock.com/image-photo/closeup-portrait-happy-confident-attractive-260nw-1955490169.jpg" />
                       </div>
                     </div>
-                    <p>
-                      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Architecto laboriosam deleniti aperiam ab veniam sint non cumque quis tempore cupiditate. Sint libero voluptas veniam at reprehenderit, veritatis harum et rerum.
-                    </p>
+
+                    <div className='ml-3'>
+                      <p className='text-lg font-medium leading-tight'>Dan Kyungu</p>
+                      <small>Foreigner</small>
+                    </div>
                   </div>
+                  <p>
+                  “Their work is absolutely inspiring. Her pieces have a mystic quality that transports you to magical places. The movement and textures in her paintings are exciting, calming and intriguing – all at the same time. We have worked with Jessica on a number of commissions, and she’s surpassed our expectations every time.”
+                  </p>
                 </div>
-                <div className='card bg-primary h-10'></div>
-                <div className='card bg-warning h-10'></div>
               </div>
+              <div className='card bg-primary h-10'></div>
+              <div className='card bg-warning h-10'></div>
             </div>
+          </div>
 
-            <div className='block mb-8 lg:mb-10'>
-              <div className='stack'>
-                <div className="card bg-secondary text-secondary-content">
-                  <div className="card-body gap-4">
-                    <div className='flex items-center'>
-                      <div className="avatar flex-shrink-0">
-                        <div className="w-16 rounded-full border-2 border-white">
-                          <img src="https://placeimg.com/96/96/people" />
-                        </div>
-                      </div>
-
-                      <div className='ml-3'>
-                        <p className='text-lg font-medium leading-tight'>Dan Kyungu</p>
-                        <small>Mobile dev</small>
+          <div className='block mb-8 lg:mb-10'>
+            <div className='stack'>
+              <div className="card bg-secondary text-secondary-content">
+                <div className="card-body gap-4">
+                  <div className='flex items-center'>
+                    <div className="avatar flex-shrink-0">
+                      <div className="w-16 rounded-full border-2 border-white">
+                        <img src="https://media.istockphoto.com/id/1145045307/photo/portrait-of-a-happy-mature-black-businessman.jpg?s=612x612&w=0&k=20&c=tMwV7b94hTyY7Cg13d_oRjeIOoZXTTOoSPZhbW2cphQ=" />
                       </div>
                     </div>
-                    <p>
-                      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Architecto laboriosam deleniti aperiam ab veniam sint non cumque quis tempore cupiditate. Sint libero voluptas veniam at reprehenderit, veritatis harum et rerum.
-                    </p>
-                  </div>
-                </div>
-                <div className='card bg-primary h-10'></div>
-                <div className='card bg-warning h-10'></div>
-              </div>
-            </div>
 
-            <div className='block mb-8 lg:mb-10'>
-              <div className='stack'>
-                <div className="card bg-secondary text-secondary-content">
-                  <div className="card-body gap-4">
-                    <div className='flex items-center'>
-                      <div className="avatar flex-shrink-0">
-                        <div className="w-16 rounded-full border-2 border-white">
-                          <img src="https://placeimg.com/95/95/people" />
-                        </div>
-                      </div>
-
-                      <div className='ml-3'>
-                        <p className='text-lg font-medium leading-tight'>Randy Doe</p>
-                        <small>Mobile dev</small>
-                      </div>
+                    <div className='ml-3'>
+                      <p className='text-lg font-medium leading-tight'>Randy Doe</p>
+                      <small>Owner</small>
                     </div>
-                    <p>
-                      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Architecto laboriosam deleniti aperiam ab veniam sint non cumque quis tempore cupiditate. Sint libero voluptas veniam at reprehenderit, veritatis harum et rerum.
-                    </p>
                   </div>
+                  <p>
+                  “The exhibit showcased works brimming with depth and emotion, highlighting Jessica’s undoubted talent. ‘Passion’, a 6 panelled masterpiece of intensity, colour and mystery was the pinnacle of an extraordinary collection, intensified only by the iconic view across the Thames to St. Paul’s Cathedral.Their works are thought provoking and serene, powerful and rich.”
+                  </p>
                 </div>
-                <div className='card bg-primary h-10'></div>
-                <div className='card bg-warning h-10'></div>
               </div>
+              <div className='card bg-primary h-10'></div>
+              <div className='card bg-warning h-10'></div>
             </div>
           </div>
         </div>
-      </section>
+      </div>
+      <div className='my-10'>
+      <Footer/>
+      </div>
+    </section>
     );
 };
 
